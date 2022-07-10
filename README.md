2f34aa593aab73c7d710a0f296812d1cf0bd85d1
robcxyz <rob@geometrylabs.io>
2022-07-10 03:14:39 -0600

    add hook that is not in release

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A fixture for testing tackle providers. **DO NOT USE**
 
 This is to test tackle providers when they have been released on GH with a tag. 
+
+**If adding to this fixture, be sure to not release with the `AddedLaterReleasedFixtureHook` hook which is being tested as not being available for released providers**
--- hooks/hooks.py
+++ hooks/hooks.py
@@ -10,3 +10,14 @@ class ReleasedFixtureHook(BaseHook):
 
     def exec(self) -> str:
         return self.src
+
+
+class AddedLaterReleasedFixtureHook(BaseHook):
+    """A hook that should not be available as it has been added after release."""
+
+    hook_type: str = 'released_added_later'
+    src: str = Field(..., description="A fixture source.")
+    args: list = ['src']
+
+    def exec(self) -> str:
+        return self.src
