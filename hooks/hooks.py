4c090d4be0bc50d460f797e1395b9c4ea7f9609f
robcxyz <rob@sudoblock.io>
2023-11-15 13:13:07 +0800

    chore: update hooks to be same as other fixture

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -4,7 +4,7 @@ from tackle import BaseHook, Field
 class ReleasedFixtureHook(BaseHook):
     """A fixture."""
 
-    hook_name: str = 'released'
+    hook_name: str = 'python_hook'
     src: str = Field(..., description="A fixture source.")
     args: list = ['src']
 
@@ -12,12 +12,12 @@ class ReleasedFixtureHook(BaseHook):
         return self.src
 
 
-class AddedLaterReleasedFixtureHook(BaseHook):
-    """A hook that should not be available as it has been added after release."""
-
-    hook_name: str = 'released_added_later'
-    src: str = Field(..., description="A fixture source.")
-    args: list = ['src']
-
-    def exec(self) -> str:
-        return self.src
+# class AddedLaterReleasedFixtureHook(BaseHook):
+#     """A hook that should not be available as it has been added after release."""
+#
+#     hook_name: str = 'released_added_later'
+#     src: str = Field(..., description="A fixture source.")
+#     args: list = ['src']
+#
+#     def exec(self) -> str:
+#         return self.src
